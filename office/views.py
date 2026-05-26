1f053bf3604635d7e57dd435fc26fd798fa436d1
Pedro <pedroemanuelvitorin00@gmail.com>
2026-05-26 13:41:33 -0300

    adicionando template e urls/views

--- office/views.py
+++ office/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'office/index.html')
+
+def elenco(request):
+    return render(request, 'office/elenco.html')
+
+def sobre(request):
+    return render(request, 'office/sobre.html')
