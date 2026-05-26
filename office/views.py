d2aa23534013621b43af7c33f392b137e1f82dde
Nicolly <nicollysilvagomesdelima@gamil.com>
2026-05-26 17:11:17 -0300

    acrescentando música ao site

--- office/views.py
+++ office/views.py
@@ -17,12 +17,12 @@ def elenco(request):
 
 def sobre(request):
     sobre_list = [
-        {"criador" : "Pedro Emanuel Vitorino Dias Monteiro", "idade" : 17, "curiosidade" : "Fala inglês fluentemente"},
-        {"criador" : "Nicolly Silva Gomes de Lima", "idade" : 16, "curiosidade" : "Tem um cachorrinho chamado Lucca"},
+        {"criador" : "Pedro Emanuel Vitorino Dias Monteiro", "idade" : 17, "curiosidade" : "Fala inglês fluentemente" },
+        {"criador" : "Nicolly Silva Gomes de Lima", "idade" : 16, "curiosidade" : "Tem um cachorrinho chamado Lucca", },
     ]
 
     context= {
         "sobre": sobre_list,
     }
-    
+
     return render(request, 'office/sobre.html', context)
