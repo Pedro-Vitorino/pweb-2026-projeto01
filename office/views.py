cf4f89e73c615cb85a63f15720a8c8cff6d4aa12
Nicolly <nicollysilvagomesdelima@gamil.com>
2026-05-26 15:58:20 -0300

    página sobre, primeiros detalhes

--- office/views.py
+++ office/views.py
@@ -7,4 +7,13 @@ def elenco(request):
     return render(request, 'office/elenco.html')
 
 def sobre(request):
-    return render(request, 'office/sobre.html')
+    sobre_list = [
+        {"criador" : "Pedro Emanuel Vitorino Dias Monteiro", "idade" : 17, "curiosidade" : "Fala inglês fluentemente"},
+        {"criador" : "Nicolly Silva Gomes de Lima", "idade" : 16, "curiosidade" : "Tem um cachorrinho chamado Lucca"},
+    ]
+
+    context= {
+        "sobre": sobre_list,
+    }
+    
+    return render(request, 'office/sobre.html', context)
